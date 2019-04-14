ea01181961d718c673951cb0f26663080c22c272
Jakub Drgoň <drgonjak@gmail.com>
2019-04-14 21:23:04 +0200

    Change toast design

--- src/js/copyHelper.js
+++ src/js/copyHelper.js
@@ -1,15 +1,29 @@
 (function() {
 	$(document).ready(function () {
-		new ClipboardJS('.include .options-group .insert-group', {
+		var switchClipboard = new ClipboardJS('.include .options-group .insert-group', {
 			text: function(trigger) {
 				return $('.include .options-group .insert-group .insert-text').text();
 			}
 		});
 
-		new ClipboardJS('.section-4 .row>div>div', {
+		switchClipboard.on('success', function() {
+			toastr.options.iconClass = 'toast-success';
+			toastr.options.timeOut = 2000;
+			toastr.options.preventDuplicates = true;
+			toastr.success('Copied');
+		});
+
+		var logoClipboard = new ClipboardJS('.section-4 .row>div>div', {
 			text: function(trigger) {
 				return '<i class="pr ' + $('.logo-title', trigger).text() + '"></i>';
 			}
+		});
+
+		logoClipboard.on('success', function() {
+			toastr.options.iconClass = 'toast-success';
+			toastr.options.timeOut = 2000;
+			toastr.options.preventDuplicates = true;
+			toastr.success('Copied');
 		});
 	});
 })();
