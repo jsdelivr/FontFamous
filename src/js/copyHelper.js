7337edeca6697f92720b33826f3788268729d25c
Jakub Drgoň <drgonjak@gmail.com>
2019-04-14 19:10:59 +0200

    Improve copy to clipboard

--- src/js/copyHelper.js
+++ src/js/copyHelper.js
@@ -1,20 +1,15 @@
 (function() {
-	function copyToClipboard(text) {
-		var $temp = $("<input style=\"opacity: 0\">");
-		$("body").append($temp);
-		$temp.val(text).select();
-		document.execCommand("copy");
-		$temp.remove();
-		toastr.success('Copied');
-	}
-
 	$(document).ready(function () {
-		$('.include .options-group .insert-group').click(function () {
-			copyToClipboard($('.include .options-group .insert-group .insert-text').text());
+		new ClipboardJS('.include .options-group .insert-group', {
+			text: function(trigger) {
+				return $('.include .options-group .insert-group .insert-text').text();
+			}
 		});
 
-		$('.section-4 .row>div>div').click(function () {
-			copyToClipboard('<i class="pr ' + $('.logo-title', this).text() + '"></i>');
+		new ClipboardJS('.section-4 .row>div>div', {
+			text: function(trigger) {
+				return '<i class="pr ' + $('.logo-title', trigger).text() + '"></i>';
+			}
 		});
 	});
 })();
